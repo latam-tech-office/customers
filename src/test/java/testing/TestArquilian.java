ab5afec691d6461db28201ef6d56565d63c8c884
Mauricio Leal <maltron@gmail.com>
2017-11-25 18:24:47 -0200

    Added some WildFly embedeed container on Arquillian

--- src/test/java/testing/TestArquilian.java
+++ src/test/java/testing/TestArquilian.java
@@ -6,11 +6,10 @@ import java.util.logging.Logger;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
 
 /**
  *
@@ -22,16 +21,52 @@ public class TestArquilian implements Serializable {
     private static final Logger LOG = Logger.getLogger(TestArquilian.class.getName());
     
     @Deployment
-    public static JavaArchive createDeployment() {
-        return ShrinkWrap.create(JavaArchive.class).addClass(Service.class);
+    public static WebArchive createDeployment() {
+        return ShrinkWrap.create(WebArchive.class).addClass(Service.class);
     }
+    
+//    @ArquillianResource
+//    private URL deploymentURL;
+//    
+//    @Deployment
+//    public static WebArchive createDeployment() {
+//        return ShrinkWrap.create(WebArchive.class)
+//                .addPackage(Resource.class.getPackage())
+//                .addClass(RESTEnabled.class).addClass(Service.class).addClass(Resource.class);
+//    }
+    
     
 
     @Test
     public void testArquilian() {
         System.out.printf("TTT testArquilian()\n");
         
-        fail("### testArquilian()");
+//        fail("### testArquilian()");
     }
+    
+//    @Test
+//    public void testResource(@ArquillianResteasyResource Resource resource) {
+//        System.out.printf(">>> TTTT testResource()\n");
+//        
+//        // Given
+//        String name = "Mauricio Leal";
+//        // When
+//        Response response = resource.myName();
+//        // Then
+//        assertEquals(name, response.getEntity().toString());
+//    }
+//    
+//    @Test
+//    @RunAsClient
+//    public void testResourceWebTarget(@ArquillianResteasyResource("api") ResteasyWebTarget target) {
+//        System.out.printf(">>> TTTT testResourceWebTarget()\n");
+//        System.out.printf(">>> WebTarget is NULL ? %s\n", target == null ? "NULL" : "NOT NULL");
+//        assertNotNull(target);
+//        
+//        String name = "Mauricio Leal";
+//        String content = target.path("/resource").request(MediaType.TEXT_PLAIN).get(String.class);
+//        assertEquals(name, content);
+//    }
+    
 
 }
